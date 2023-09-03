0e287faffa2bf7caa16b781020e4ada18016695d
RealPharao <patrickpharao@gmail.com>
2023-09-03 21:13:53 +0200

    id urlParams

--- js/product.js
+++ js/product.js
@@ -31,7 +31,10 @@
 //   // producktInfo(".brandimage").src = urlImg;
 // }
 
-fetch("https://kea-alt-del.dk/t7/api/products/1525")
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+fetch("https://kea-alt-del.dk/t7/api/products/" + id)
   .then((response) => response.json())
   .then((data) => showProduct(data));
 
